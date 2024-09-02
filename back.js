40ec57d29f4f073e4891efcf46340d31c7e906f6
ArthurBlasi <rthrblasi@gmail.com>
2024-09-02 14:22:46 -0300

    projeto finalizado

--- back.js
+++ back.js
@@ -1,10 +1,12 @@
 function insert(num)
 {
     var numero = document.getElementById('resultado').innerHTML;
-    if(numero.charAt(0)) {
+
+    if (numero === "0") {
         document.getElementById('resultado').innerHTML = num;
+    } else {
+        document.getElementById('resultado').innerHTML += num;
     }
-    document.getElementById('resultado').innerHTML = numero + num;
 }
 
 function cleaning(num)
@@ -51,73 +53,25 @@ function addition(add)
 
 function plusminus()
 {
-    /*var numero = document.getElementById('resultado').innerHTML;
+    let expression = document.getElementById('resultado').innerHTML;
 
-    for (let i = numero.length; i > 0; i--) {
-        if (numero.charAt(i) === "+" || numero.charAt(i) === "-" || numero.charAt(i) === "×" || numero.charAt(i) === "÷") {
-
-            if(numero.charAt(i) === "-") {
-                numero.charAt(i+1) = "+";
-                break;
-            } else {
-                numero.charAt(i+1) = "-";
-            }
-        }
-        numero.charAt(i+1) = numero.charAt(i);
-    }*/
-
-        /*let expression = document.getElementById('resultado').innerHTML;
-
-        let lastOperatorIndex = Math.max(
-            expression.lastIndexOf('+'),
-            expression.lastIndexOf('-'),
-            expression.lastIndexOf('×'),
-            expression.lastIndexOf('÷')
-        );
-    
-        let lastNumber = expression.substring(lastOperatorIndex + 1);
-    
-        if (lastNumber.startsWith('-')) {
-            lastNumber = lastNumber.substring(1);
-        } else {
-            lastNumber = '-' + lastNumber;
-        }
-    
-        expression = expression.substring(0, lastOperatorIndex + 1) + lastNumber;
-        document.getElementById('resultado').innerHTML = expression;*/
-
-        let expression = document.getElementById('resultado').innerHTML;
-
-    // Localiza o índice do último operador matemático (+, -, ×, ÷)
     let lastOperatorIndex = Math.max(
         expression.lastIndexOf('+'),
-        expression.lastIndexOf('-'),
+        expression.lastIndexOf('−'),
         expression.lastIndexOf('×'),
         expression.lastIndexOf('÷')
     );
 
-    // Extrai a parte da expressão após o último operador
     let lastNumber = expression.substring(lastOperatorIndex + 1);
 
     if (lastNumber.startsWith('-')) {
-        // Se o último número for negativo, remova o sinal de negativo
         lastNumber = lastNumber.substring(1);
-
-        // Verifica o operador anterior e ajusta a expressão
-        if (expression.charAt(lastOperatorIndex) === '-') {
-            expression = expression.substring(0, lastOperatorIndex) + '+' + lastNumber;
-        } else {
-            expression = expression.substring(0, lastOperatorIndex + 1) + lastNumber;
-        }
     } else {
-        // Se o último número for positivo, adicione o sinal de negativo
         lastNumber = '-' + lastNumber;
-        expression = expression.substring(0, lastOperatorIndex + 1) + lastNumber;
     }
 
-    // Atualiza o display com a nova expressão
+    expression = expression.substring(0, lastOperatorIndex + 1) + lastNumber;
     document.getElementById('resultado').innerHTML = expression;
-        
 }
 
 function virgula(vir)
@@ -138,38 +92,6 @@ function equals()
     } catch (e) {
         alert('NaN');
     }
-
-    /*try {
-        const result = eval(document.getElementById('resultado').value);
-        document.getElementById('resultado').value = result;
-    } catch (e) {
-        alert('Expressão Inválida');
-    }
-
-
-    var numero = document.getElementById('resultado').innerHTML;
-    let encontrou = false;
-    var x = 0;
-    var num1;
-    var num2;
-
-    for (let i = 0; i < numero.length; i++) {
-        if (numero.charAt(i) === "+") {
-          encontrou = true;
-          x = i;
-          break;
-        }
-    }
-
-    for (let i = 0; i < x; i++) {
-        num1 += charAt(i);
-    }
-    
-    for (let i = x+1; i < numero.length; i++) {
-        num2 += charAt(i);
-    }
-
-    document.getElementById('resultado').innerHTML = num1 + num2;*/
 }
 
 
