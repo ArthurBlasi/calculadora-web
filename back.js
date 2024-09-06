c41c19e97d7d34925115c234d64f438d065768ec
ArthurBlasi <rthrblasi@gmail.com>
2024-09-06 11:02:51 -0300

    Comentários. Projeto finalizado.

--- back.js
+++ back.js
@@ -1,5 +1,6 @@
 function insert(num)
 {
+    //função responsável por inserir um número no display da calculadora (0-9)
     var numero = document.getElementById('resultado').innerHTML;
 
     if (numero === "0") {
@@ -11,52 +12,46 @@ function insert(num)
 
 function cleaning(num)
 {
+    //função responsável por limpar os displays do resultado e do "histórico"/calculo atual
     document.getElementById('resultado').innerHTML = num;
     document.getElementById('historico').innerHTML = "";
 }
 
 function erase(num)
 {
+    //função responsável por apagar apenas o display do resultado
     document.getElementById('resultado').innerHTML = num;
 }
 
 function percentage()
 {
+    //essa função lê o valor do display, divide ele por 100 e adiciona um "%" no histórico
     var numero = document.getElementById('resultado').innerHTML;
     document.getElementById('resultado').innerHTML = numero / 100;
     document.getElementById('historico').innerHTML = numero + "%";
 }
 
-function division(div)
+function operations(sinal)
 {
+    //função responsável por inserir um operador (+, −, ×, ÷), mas sempre verificando
+    //se a expressão já possui um sinal de =, pois causaria erro na função equals
     var numero = document.getElementById('historico').innerHTML + document.getElementById('resultado').innerHTML;
+
+    let lastOperatorIndex = numero.lastIndexOf('=');
+
+    if (lastOperatorIndex !== -1) {
+        let ultimoNumero = numero.substring(lastOperatorIndex + 1);
+        document.getElementById('historico').innerHTML = ultimoNumero + sinal;
+    } else {
+        document.getElementById('historico').innerHTML = numero + sinal;
+    }
     document.getElementById('resultado').innerHTML = "";
-    document.getElementById('historico').innerHTML = numero + div;
 }
 
-function multiplication(mult)
-{
-    var numero = document.getElementById('historico').innerHTML + document.getElementById('resultado').innerHTML;
-    document.getElementById('resultado').innerHTML = "";
-    document.getElementById('historico').innerHTML = numero + mult;
-}
-
-function subtraction(sub)
-{
-    var numero = document.getElementById('historico').innerHTML + document.getElementById('resultado').innerHTML;
-    document.getElementById('resultado').innerHTML = "";
-    document.getElementById('historico').innerHTML = numero + sub;
-}
-
-function addition(add)
-{
-    var numero = document.getElementById('historico').innerHTML + document.getElementById('resultado').innerHTML;
-    document.getElementById('resultado').innerHTML = "";
-    document.getElementById('historico').innerHTML = numero + add;
-}
 
 function plusminus()
 {
+    // função responsável por trocar o sinal do último número
     let numero = document.getElementById('historico').innerHTML + document.getElementById('resultado').innerHTML;
 
     let lastOperatorIndex = Math.max(
@@ -74,25 +69,27 @@ function plusminus()
         ultimoNumero = '-' + ultimoNumero;
     }
 
-    //numero = numero.substring(0, lastOperatorIndex + 1) + ultimoNumero;
     document.getElementById('resultado').innerHTML = ultimoNumero;
 }
 
 function virgula(vir)
 {
+    //função responsável por colocar a vírgula no display
     var numero = document.getElementById('resultado').innerHTML;
     document.getElementById('resultado').innerHTML = numero + vir;
 }
 
 function equals()
 {
+    //função responsável por ler a expressão completa e realizar o cálculo
+    //o método eval() serve para avaliar uma expressão de código JavaScript que está representada como uma string
     try{
-        let expression = document.getElementById('historico').innerHTML + document.getElementById('resultado').innerHTML;
-        expression = expression.replaceAll('÷', '/').replaceAll('×', '*');
-        const result = eval(expression);
-        expression = expression.replaceAll('/', '÷').replaceAll('*', '×');
-        document.getElementById('historico').innerHTML = expression;
-        document.getElementById('resultado').innerHTML = result;
+        let expressao = document.getElementById('historico').innerHTML + document.getElementById('resultado').innerHTML;
+        expressao = expressao.replaceAll('÷', '/').replaceAll('×', '*');
+        const resultado = eval(expressao);
+        expressao = expressao.replaceAll('/', '÷').replaceAll('*', '×');
+        document.getElementById('historico').innerHTML = expressao + "=";
+        document.getElementById('resultado').innerHTML = resultado;
     } catch (e) {
         alert('Expressão inválida.');
     }
